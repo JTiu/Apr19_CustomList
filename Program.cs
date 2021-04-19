e5005a23a7186d3fe4d3d73b16d7e0d3b5a9d711
James Tiu <jamestiulawyer@gmail.com>
2021-04-19 14:50:30 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Apr19_CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is My April 19 Custome List!");
+        }
+    }
+}
